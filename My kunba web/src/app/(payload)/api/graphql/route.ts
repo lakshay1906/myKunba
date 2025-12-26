743b0c0956f70641d5319473a656d441f25d8e83
lakshay1906 <lbhati020@gmail.com>
2025-12-27 01:12:40 +0530

    all routes updated

--- My kunba web/src/app/(payload)/api/graphql/route.ts
+++ My kunba web/src/app/(payload)/api/graphql/route.ts
@@ -1,5 +1,7 @@
 /* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
 /* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+export const dynamic = 'force-dynamic'
+
 import config from '@payload-config'
 import { GRAPHQL_POST, REST_OPTIONS } from '@payloadcms/next/routes'
 
