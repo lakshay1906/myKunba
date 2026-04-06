f9f4f033ca6e77b08c0acb4153a18e4f2fefbfed
lakshay1906 <lbhati020@gmail.com>
2026-04-06 12:10:48 +0530

    blog detail page updates

--- My kunba web/src/components/Blog/BlogContent.tsx
+++ My kunba web/src/components/Blog/BlogContent.tsx
@@ -120,7 +120,7 @@ function InternalLinksCards({
     >
       <div className="mb-8">
         <h2 className="text-xl font-semibold mb-4">Related Content</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {posts.map((post) => (
             <BlogCard key={post.id} post={post} />
           ))}
